2a6b04cd8e4cd500aad8b5ea0cbe6f2e97a148c4
Anders Soh <anders.soh@circles.asia>
2023-08-23 09:58:26 +0800

    update list widget with base widget composable

--- app/src/main/java/com/example/composesdui/ui/sdengine/widget/Base.kt
+++ app/src/main/java/com/example/composesdui/ui/sdengine/widget/Base.kt
@@ -7,17 +7,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.composesdui.api.model.widget.ListWidget
-import com.example.composesdui.ui.sdengine.ComponentEngine
+import com.example.composesdui.api.model.widget.BaseWidget
 import com.example.composesdui.ui.theme.Typography
 
 @Composable
-fun ListWidgetComposable(widget: ListWidget) {
-    BaseWidgetComposable(widget) {
-        Column {
-            widget.components.forEach { component ->
-                ComponentEngine(component)
-            }
-        }
+fun BaseWidgetComposable(widget: BaseWidget, content: @Composable () -> Unit) {
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text( text = widget.header?.title ?: "", style = Typography.titleLarge)
+        Spacer(modifier = Modifier.padding(1.dp))
+        content()
     }
 }
